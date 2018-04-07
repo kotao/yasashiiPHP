c3b9a9e66f629b18cfef1d1392abd733e125f072
kotao <kota.16000816@gmail.com>
2018-04-07 20:55:31 +0900

    first commit

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCUTYPE html>
+<html lang="ja">
+<head>
+<meta=charset="UTF-8">
+<title>入力フォーム</title>
+</head>
+<body>
+入力フォーム<br>
+<form method="post" action="add.php">
+料理名:<input type="text" name="recipe name" required>
+<input type="submit" value="送信">
+</form>
+</body>
+</html>
